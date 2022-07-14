1e99b323daba1cc74e029a913ff78161b1e37ae9
johathanx <johathanx@outlook.com>
2022-07-14 16:41:47 -0300

    anotacoes JPA para classes de dominio

--- src/main/java/com/jonathan/hepldesk/domain/Cliente.java
+++ src/main/java/com/jonathan/hepldesk/domain/Cliente.java
@@ -1,5 +1,37 @@
 package com.jonathan.hepldesk.domain;
 
-public class Cliente {
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+
+import com.jonathan.hepldesk.domain.enums.Perfil;
+
+@Entity
+public class Cliente extends Pessoa{
+	private static final long serialVersionUID = 1L;
+	
+	@OneToMany(mappedBy = "cliente")
+	private List<Chamado> chamados = new ArrayList<>();
+
+	public Cliente() {
+		super();
+		addPerfil(Perfil.CLIENTE);
+	}
+
+	public Cliente(Integer id, String nome, String cpf, String email, String senha) {
+		super(id, nome, cpf, email, senha);
+		addPerfil(Perfil.CLIENTE);
+	}
+
+	public List<Chamado> getChamados() {
+		return chamados;
+	}
+
+	public void setChamados(List<Chamado> chamados) {
+		this.chamados = chamados;
+	}
+	
+	
 }
--- src/main/java/com/jonathan/hepldesk/domain/Tecnico.java
+++ src/main/java/com/jonathan/hepldesk/domain/Tecnico.java
@@ -1,5 +1,37 @@
 package com.jonathan.hepldesk.domain;
 
-public class Tecnico {
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+
+import com.jonathan.hepldesk.domain.enums.Perfil;
+
+@Entity
+public class Tecnico extends Pessoa{
+	private static final long serialVersionUID = 1L;
+	
+	@OneToMany(mappedBy = "tecnico")
+	private List<Chamado> chamados = new ArrayList<>();
+
+	public Tecnico() {
+		super();
+		addPerfil(Perfil.CLIENTE);
+	}
+
+	public Tecnico(Integer id, String nome, String cpf, String email, String senha) {
+		super(id, nome, cpf, email, senha);
+		addPerfil(Perfil.CLIENTE);
+	}
+
+	public List<Chamado> getChamados() {
+		return chamados;
+	}
+
+	public void setChamados(List<Chamado> chamados) {
+		this.chamados = chamados;
+	}
+	
+	
 }
