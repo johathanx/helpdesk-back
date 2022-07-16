9cac0ba96dd20568a64a020e2ba6ea4106f91ff5
johathanx <johathanx@outlook.com>
2022-07-16 17:54:12 -0300

    Exceção para objeto não encontrado

--- src/main/java/com/jonathan/hepldesk/services/TecnicoService.java
+++ src/main/java/com/jonathan/hepldesk/services/TecnicoService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.jonathan.hepldesk.domain.Tecnico;
 import com.jonathan.hepldesk.repositories.TecnicoRepository;
+import com.jonathan.hepldesk.services.exceptions.ObjectnotFoundException;
 
 @Service
 public class TecnicoService {
@@ -17,7 +18,7 @@ public class TecnicoService {
 	public Tecnico findById(Integer id) {
 		
 		Optional<Tecnico> obj = repository.findById(id);
-		return obj.orElse(null);
-	
+		return obj.orElseThrow(() -> new ObjectnotFoundException("Objeto não encontrado! Id: "+ id ));
+	 
 	}
 }
