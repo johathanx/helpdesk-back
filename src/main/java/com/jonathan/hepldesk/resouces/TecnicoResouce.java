d81f37931872d15b4203d6d9f6e2713116cd20a8
johathanx <johathanx@outlook.com>
2022-07-16 17:00:02 -0300

    Padrão DTO para Tecnico

--- src/main/java/com/jonathan/hepldesk/resouces/TecnicoResouce.java
+++ src/main/java/com/jonathan/hepldesk/resouces/TecnicoResouce.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.jonathan.hepldesk.domain.Tecnico;
+import com.jonathan.hepldesk.domain.dtos.TecnicoDTO;
 import com.jonathan.hepldesk.services.TecnicoService;
 
 @RestController
@@ -21,9 +22,9 @@ public class TecnicoResouce {
 	private TecnicoService service;
 	
 	@GetMapping (value = "/{id}")
-	public ResponseEntity<Tecnico> findById(@PathVariable Integer id) {
+	public ResponseEntity<TecnicoDTO> findById(@PathVariable Integer id) {
 		 Tecnico obj = service.findById(id);
-		 return ResponseEntity.ok().body(obj);
-		
+		 return ResponseEntity.ok().body(new TecnicoDTO(obj));
+		 
 	}
 }
