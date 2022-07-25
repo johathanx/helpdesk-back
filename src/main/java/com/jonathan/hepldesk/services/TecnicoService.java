2509b57de5fcc777026da3dce4bfb4ff5793e725
johathanx <johathanx@outlook.com>
2022-07-25 17:04:56 -0300

    delete para Tecnico implementado

--- src/main/java/com/jonathan/hepldesk/services/TecnicoService.java
+++ src/main/java/com/jonathan/hepldesk/services/TecnicoService.java
@@ -49,6 +49,15 @@ public class TecnicoService {
 		oldObj = new Tecnico(objDTO);
 		return repository.save(oldObj);
 	}
+	
+    public void delete(Integer id) {
+		Tecnico obj = findById(id);
+		if(obj.getChamados().size() > 0) {
+			throw new DataIntegrityViolationException("Tecnico possui ordens de serviço e não pode ser deletado!");
+		}
+		
+		repository.deleteById(id);
+	}
 
 	private void validaPorCpfEEmail(TecnicoDTO objDTO) {
 
@@ -62,6 +71,8 @@ public class TecnicoService {
 			throw new DataIntegrityViolationException("E-mail já cadastrado no Sistema!");
 		}
 	}
+
+	
 
 	
 
