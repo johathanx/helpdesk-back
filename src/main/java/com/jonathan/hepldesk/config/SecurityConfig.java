12527fdce90324977d80e5e222b1d71f5987f880
johathanx <johathanx@outlook.com>
2022-08-03 15:31:31 -0300

    autorizacao com JWT

--- src/main/java/com/jonathan/hepldesk/config/SecurityConfig.java
+++ src/main/java/com/jonathan/hepldesk/config/SecurityConfig.java
@@ -18,6 +18,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.jonathan.hepldesk.security.JWTAuthenticationFilter;
+import com.jonathan.hepldesk.security.JWTAuthorizationFilter;
 import com.jonathan.hepldesk.security.JWTUtil;
 
 
@@ -42,6 +43,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 		http.cors().and().csrf().disable();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.authorizeRequests().antMatchers(PUBLIC_MATCHERS).permitAll().anyRequest().authenticated();
 
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
